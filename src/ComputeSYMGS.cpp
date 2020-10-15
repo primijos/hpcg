ffa409f82d8f9d4fc2617876e0e3a2e0758e614a
José Oliver Segura <primijos@gmail.com>
2020-10-15 11:43:25 +0200

    Added blocking to SYMGS

--- src/ComputeSYMGS.cpp
+++ src/ComputeSYMGS.cpp
@@ -61,77 +61,86 @@ int ComputeSYMGS( const SparseMatrix & A, const Vector & r, Vector & x) {
 	int * i_before_diagonal = ((int **)A.optimizationData)[1];
 	int * i_after_diagonal = ((int **)A.optimizationData)[2];
 
+	int block_size=1024;
+	for (local_int_t ii=0; ii<nrow; ii+=block_size) {
+		local_int_t nnrow = ii+block_size;
+		nnrow = nnrow < nrow ? nnrow : nrow;
 #ifndef HPCG_NO_OPENMP
 	#pragma omp parallel for
 #endif
-  for (local_int_t i=0; i< nrow; i++) {
-    const double * const currentValues = A.matrixValues[i];
-    const local_int_t * const currentColIndices = A.mtxIndL[i];
-    const int nnz_to_the_left = i_before_diagonal[i];
-		const int currentNumberOfNonzeros = A.nonzerosInRow[i];
-
-    double sum = rv[i]; // RHS value
-
-    for (int j=nnz_to_the_left+1; j < currentNumberOfNonzeros; j++) {
-      local_int_t curCol = currentColIndices[j];
-      sum -= currentValues[j] * xv[curCol];
-    }
-
-    xv_tmp[i] = sum;
-
-  }
-
-  for (local_int_t i=0; i< nrow; i++) {
-    const double * const currentValues = A.matrixValues[i];
-    const local_int_t * const currentColIndices = A.mtxIndL[i];
-    const int nnz_to_the_left = i_before_diagonal[i];
-    const double  currentDiagonal = matrixDiagonal[i][0]; // Current diagonal value
-    double sum = xv_tmp[i]; // RHS value
-
-    for (int j=0; j< nnz_to_the_left; j++) {
-      local_int_t curCol = currentColIndices[j];
-      sum -= currentValues[j] * xv[curCol];
-    }
-
-    xv[i] = sum/currentDiagonal;
-
-  }
+		for (local_int_t i=ii; i< nnrow; i++) {
+			const double * const currentValues = A.matrixValues[i];
+			const local_int_t * const currentColIndices = A.mtxIndL[i];
+			const int nnz_to_the_left = i_before_diagonal[i];
+			const int currentNumberOfNonzeros = A.nonzerosInRow[i];
+	
+			double sum = rv[i]; // RHS value
+	
+			for (int j=nnz_to_the_left+1; j < currentNumberOfNonzeros; j++) {
+				local_int_t curCol = currentColIndices[j];
+				sum -= currentValues[j] * xv[curCol];
+			}
+	
+			xv_tmp[i] = sum;
+	
+		}
+	
+		for (local_int_t i=ii; i< nnrow; i++) {
+			const double * const currentValues = A.matrixValues[i];
+			const local_int_t * const currentColIndices = A.mtxIndL[i];
+			const int nnz_to_the_left = i_before_diagonal[i];
+			const double  currentDiagonal = matrixDiagonal[i][0]; // Current diagonal value
+			double sum = xv_tmp[i]; // RHS value
+	
+			for (int j=0; j< nnz_to_the_left; j++) {
+				local_int_t curCol = currentColIndices[j];
+				sum -= currentValues[j] * xv[curCol];
+			}
+	
+			xv[i] = sum/currentDiagonal;
+	
+		}
+	}
 
   // Now the back sweep.
 	//
+	for (local_int_t ii=nrow-1; ii>=0; ii-=block_size) {
+		local_int_t low_lim = ii-block_size;
+		low_lim = low_lim < 0 ? 0 : low_lim;
 #ifndef HPCG_NO_OPENMP
 	#pragma omp parallel for
 #endif
-  for (local_int_t i=0; i<nrow; i++) {
-    const double * const currentValues = A.matrixValues[i];
-    const local_int_t * const currentColIndices = A.mtxIndL[i];
-    const int nnz_to_the_left = i_before_diagonal[i];
-    const int currentNumberOfNonzeros = A.nonzerosInRow[i];
-    double sum = rv[i]; // RHS value
-
-    for (int j = 0; j< nnz_to_the_left; j++) {
-      local_int_t curCol = currentColIndices[j];
-      sum -= currentValues[j]*xv[curCol];
-    }
-
-    xv_tmp[i] = sum;
-  }
-
-  for (local_int_t i=nrow-1; i>=0; i--) {
-    const double * const currentValues = A.matrixValues[i];
-    const local_int_t * const currentColIndices = A.mtxIndL[i];
-    const int nnz_to_the_left = i_before_diagonal[i];
-    const int currentNumberOfNonzeros = A.nonzerosInRow[i];
-    const double  currentDiagonal = matrixDiagonal[i][0]; // Current diagonal value
-    double sum = xv_tmp[i]; // RHS value
-
-    for (int j = nnz_to_the_left + 1; j< currentNumberOfNonzeros; j++) {
-      local_int_t curCol = currentColIndices[j];
-      sum -= currentValues[j]*xv[curCol];
-    }
-
-    xv[i] = sum/currentDiagonal;
-  }
+		for (local_int_t i=ii; i>=low_lim; i--) {
+			const double * const currentValues = A.matrixValues[i];
+			const local_int_t * const currentColIndices = A.mtxIndL[i];
+			const int nnz_to_the_left = i_before_diagonal[i];
+			const int currentNumberOfNonzeros = A.nonzerosInRow[i];
+			double sum = rv[i]; // RHS value
+	
+			for (int j = 0; j< nnz_to_the_left; j++) {
+				local_int_t curCol = currentColIndices[j];
+				sum -= currentValues[j]*xv[curCol];
+			}
+	
+			xv_tmp[i] = sum;
+		}
+	
+		for (local_int_t i=ii; i>=low_lim; i--) {
+			const double * const currentValues = A.matrixValues[i];
+			const local_int_t * const currentColIndices = A.mtxIndL[i];
+			const int nnz_to_the_left = i_before_diagonal[i];
+			const int currentNumberOfNonzeros = A.nonzerosInRow[i];
+			const double  currentDiagonal = matrixDiagonal[i][0]; // Current diagonal value
+			double sum = xv_tmp[i]; // RHS value
+	
+			for (int j = nnz_to_the_left + 1; j< currentNumberOfNonzeros; j++) {
+				local_int_t curCol = currentColIndices[j];
+				sum -= currentValues[j]*xv[curCol];
+			}
+	
+			xv[i] = sum/currentDiagonal;
+		}
+	}
 
   return 0;
 }
